142707dba166a31a191e561201f7f4cc4e7bdbb5
Marcin <grzechnikmarcin@wp.pl>
2025-11-18 19:04:27 +0100

    Add Crew model

--- airport/models.py
+++ airport/models.py
@@ -21,3 +21,15 @@ class AirplaneType(models.Model):
     class Meta:
         db_table = "airplane_type"
         ordering = ["name"]
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        db_table = "crew"
+        ordering = ["last_name"]
