cff3b460cfa18a306dc4d4577c0642d33b32ff05
Marcin <grzechnikmarcin@wp.pl>
2025-11-19 17:43:54 +0100

    Add Order model in booking app

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="orders",
+    )
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.user}"
+
+    class Meta:
+        db_table = "order"
+        ordering = ["-created_at"]
