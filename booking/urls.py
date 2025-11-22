4bc33958ed6f24460b04b6236c5074a7e649a7df
Marcin <grzechnikmarcin@wp.pl>
2025-11-22 22:19:18 +0100

    Add Order endpoint in booking app

---
+++ booking/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import OrderViewSet
+
+
+router = routers.DefaultRouter()
+router.register("orders", OrderViewSet)
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "booking"
--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/doc/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/", include("airport.urls")),
+    path("api/booking/", include("booking.urls")),
 ]
 
 if settings.DEBUG:
