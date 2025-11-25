897712d4680c424f895458dbe160851bf7e3794c
Marcin <grzechnikmarcin@wp.pl>
2025-11-25 13:30:03 +0100

    Separate .env files for local and Docker environments

--- airport/tests.py
+++ airport/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Airport
 
-# Create your tests here.
+
+class AirportModelTest(TestCase):
+    def test_airport_str(self):
+        airport = Airport.objects.create(
+            name="Test Airport",
+            closest_big_city="Test City",
+        )
+        self.assertEqual(str(airport), "Test Airport")
